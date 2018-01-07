ad2d31fe9bc2b45f9b77928126e2da31e01f3e42
alu0100851236 <alu0100851236@ull.edu.es>
2018-01-07 18:41:14 +0000

    usuario mongodb heroku

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//'url': 'mongodb://localhost/db_usuarios'
-	'url': 'mongodb://<dbuser>:<dbpassword>@ds247357.mlab.com:47357/heroku_drv1dp9x'
+	'url': 'mongodb://heroku_drv1dp9x:heroku_drv1dp9x@ds247357.mlab.com:47357/heroku_drv1dp9x'
 };
