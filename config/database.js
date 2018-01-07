c36a38412750dbfc0111040f37418a3020d07afc
alu0100851236 <alu0100851236@ull.edu.es>
2018-01-07 18:33:21 +0000

    base de datos de heroku

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-	'url': 'mongodb://localhost/db_usuarios'
+	//'url': 'mongodb://localhost/db_usuarios'
+	'url': 'mongodb://<dbuser>:<dbpassword>@ds247357.mlab.com:47357/heroku_drv1dp9x'
 };
