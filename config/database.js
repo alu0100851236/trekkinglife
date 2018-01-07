0901d48990238193daab2ba1259cf641a57b9e0a
alu0100851236 <alu0100851236@ull.edu.es>
2018-01-07 19:39:56 +0000

    arreglo varios

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
 	//'url': 'mongodb://localhost/db_usuarios'
-	'url': 'mongodb://heroku_drv1dp9x:heroku_drv1dp9x@ds247357.mlab.com:47357/heroku_drv1dp9x'
+	'url': 'mongodb://heroku_drv1dp9x:trekkinglife20@ds247357.mlab.com:47357/heroku_drv1dp9x'
 };
